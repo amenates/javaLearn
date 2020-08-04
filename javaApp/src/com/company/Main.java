3d073697a3095d7c6796f161624f879b07d9124f
Julia Nikolaeva <julia.nikolaeva@myoffice.team>
2020-08-04 17:42:42 +0300

    test

---
+++ javaApp/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.lang.String;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        System.out.println("Hello world!");
+//        System.out.print("Hello world!");
+
+//        // Переменные:
+//        byte num = 23;
+//        int num_1 = 446465;
+//
+//        float num_2 = 23.45f; // число с плавающей точкой, обязательно использовать в конце f
+//        double num_3 = 123.23124212412d; // для очень больших чисел, обязательно использовать в конце d
+//
+//        boolean isTrue = true; // true == 1, false == 0
+//        boolean isFalse = false; // true == 1, false == 0
+//
+//        char sym = 'R'; // принимает только один символ
+//
+//        String str = "Hello World!";
+//
+//        System.out.println(num);
+//        System.out.println(num_1);
+//        System.out.println(num_2);
+//        System.out.println(num_3);
+//        System.out.println(isTrue);
+//        System.out.println(isFalse);
+//        System.out.println(sym);
+//        System.out.println(str);
+
+        // получение информации от пользователя
+        Scanner str = new Scanner(System.in);
+        System.out.println("Вы ввели: " + str.nextLine());
+
+    }
+}
